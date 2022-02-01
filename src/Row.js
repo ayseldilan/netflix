57b2e04c948b712cd6eef7781e59b545f610451d
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2022-02-01 11:00:13 +0000

    finished nav and banner

--- src/Row.js
+++ src/Row.js
@@ -1,33 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios.js";
 import './Row.css';
-// import Youtube from "react-youtube";
-const baseImgUrl = "https://image.tmdb.org/t/p/original";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
+const base_url = "https://image.tmdb.org/t/p/original";
 
-const Row = ({ title, fetchUrl, isLargeRow }) => {
-
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
 
-  // Options for react-youtube
-  const opts = {
-    height: "390",
-    width: "100%",
-    playerVars: {
-      autoplay: 1,
-    },
-  };
-  const handleClick = async (movie) => {
-    if (trailerUrl) {
-      setTrailerUrl("");
-    } else {
-      let trailerurl = await axios.get(`/movie/${movie?.id}/videos?api_key=${process.env.REACT_APP_API_KEY}`);
-      setTrailerUrl(trailerurl.data.results[0]?.key);
-    }
-  }
-  // const url = "https://api.themoviedb.org/3";
-  // const fetchUrl_full= `${url}${fetchUrl}`;
 
   useEffect(() => {
     async function fetchData() {
@@ -35,9 +17,30 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
       setMovies(request.data.results);
       return request;
     }
-    fetchData();
+    fetchData()
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = async (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+      .then((url) => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get("v"));
+      })
+      .catch((error) => console.log(error));
+    }
+  };
+  
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -45,14 +48,17 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`${baseImgUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
-            onClick={handleClick.bind(this, movie)}
+            
           />
         ))}
       </div>
-      {/* {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />} */}
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
